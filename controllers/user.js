b547c76c8e1098432c456e5cee5dcb57ae78caf3
Khaled <khaled.ali.architect@gmail>
2023-06-27 06:28:16 +0300

    Signup Controller Error Handling
    See: #4

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,20 @@
 const User = require("../models/user");
+const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.signup = (req, res) => {
   const user = new User(req.body);
-  user.save();
-  res.json({
-    user,
-  });
+  user
+    .save()
+    .then(() => {
+      user.salt = undefined;
+      user.hashed_password = undefined;
+      res.json({
+        user,
+      });
+    })
+    .catch((error) => {
+      return res.status(400).json({
+        error: errorHandler(error),
+      });
+    });
 };
