49f81a3c635d60bc681ad2102babc4d6456b5364
Khaled <khaled.ali.architect@gmail>
2023-07-02 06:11:34 +0300

    React - Creating the Home Page and Routing
    See: #19

--- client/src/Routing.js
+++ client/src/Routing.js
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./user/Signup";
 import Signin from "./user/Signin";
+import Home from "./core/Home";
 
 const Routing = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
       </Routes>
