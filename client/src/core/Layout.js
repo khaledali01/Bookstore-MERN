5d0fba37024f4fe777dc9df1d97d92b394894215
Khaled <khaled.ali.architect@gmail>
2023-07-04 15:35:55 +0300

    Fixing Bootstrap Version Issue
    See: #33

--- client/src/core/Layout.js
+++ client/src/core/Layout.js
@@ -1,26 +1,21 @@
 import React from "react";
 import Menu from "./Menu";
+import "../styles.css";
 
 const Layout = ({
-  title = "Title",
-  description = "Description",
-  className,
-  children,
-}) => {
-  return (
+    title = "Title",
+    description = "Description",
+    className,
+    children
+}) => (
     <div>
-      <Menu />
-      <div className="my-5">
-        <div className="p-5 text-center bg-body-tertiary">
-          <div className="container py-5">
-            <h1 className="text-body-emphasis">{title}</h1>
-            <p className="col-lg-8 mx-auto lead">{description}</p>
-          </div>
+        <Menu />
+        <div className="jumbotron">
+            <h2>{title}</h2>
+            <p className="lead">{description}</p>
         </div>
-      </div>
-      <div className={className}>{children}</div>
+        <div className={className}>{children}</div>
     </div>
-  );
-};
+);
 
 export default Layout;
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Routing from "./Routing";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Routing />);
