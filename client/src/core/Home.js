13b4a1785272aa0179c17a313e7ed46ccba33094
Khaled <khaled.ali.architect@gmail>
2023-07-06 02:21:02 +0300

    React - Products Showcase
    See: #39

--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -1,9 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
+import { getProducts } from "./apiCore";
+import Card from "./Card";
 
 const Home = () => {
+  const [productsBySell, setProductsBySell] = useState([]);
+  const [productsByArrival, setProductsByArrival] = useState([]);
+  const [error, setError] = useState(false);
+
+  const loadProductsBySell = () => {
+    getProducts("sold").then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductsBySell(data.productsList);
+      }
+    });
+  };
+
+  const loadProductsByArrival = () => {
+    getProducts("createdAt").then((data) => {
+      console.log(data);
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductsByArrival(data.productsList);
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadProductsByArrival();
+    loadProductsBySell();
+  }, []);
+
   return (
-    <Layout title="Home Page" description="Books E-Commerce">
+    <Layout
+      title="FullStack React Node MongoDB Ecommerce App"
+      description="Node React E-commerce App"
+      className="container-fluid"
+    >
+      <h2 className="mb-4">New Arrivals</h2>
+      <div className="row">
+        {productsByArrival.map((product, i) => (
+          <div key={i} className="col-4 mb-3">
+            <Card product={product} />
+          </div>
+        ))}
+      </div>
+
+      <h2 className="mb-4">Best Sellers</h2>
+      <div className="row">
+        {productsBySell.map((product, i) => (
+          <div key={i} className="col-4 mb-3">
+            <Card product={product} />
+          </div>
+        ))}
+      </div>
     </Layout>
   );
 };
