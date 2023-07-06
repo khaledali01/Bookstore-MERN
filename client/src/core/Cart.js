88a51d8b8d3b14ad7acf504b4795d81d85c007c9
Khaled <khaled.ali.architect@gmail>
2023-07-06 17:29:05 +0300

    React - Payment Implementation
    See: #45

--- client/src/core/Cart.js
+++ client/src/core/Cart.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import Layout from "./Layout";
 import { getCart } from "./cartHelpers";
 import Card from "./Card";
+import Checkout from "./Checkout";
 
 const Cart = () => {
   const [items, setItems] = useState([]);
@@ -52,6 +53,7 @@ const Cart = () => {
         <div className="col-6">
           <h2 className="mb-4">Your cart summary</h2>
           <hr />
+          <Checkout products={items} setRun={setRun} run={run} />
         </div>
       </div>
     </Layout>
