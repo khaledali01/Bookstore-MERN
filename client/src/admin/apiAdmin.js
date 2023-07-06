8d1676b9ba40bd0dd76aba3d36dae159af52d688
Khaled <khaled.ali.architect@gmail>
2023-07-06 17:42:05 +0300

    React - Orders Implementation
    See: #46

--- client/src/admin/apiAdmin.js
+++ client/src/admin/apiAdmin.js
@@ -37,10 +37,54 @@ export const createProduct = (userId, token, product) => {
 
 export const getCategories = () => {
   return fetch(`${API}/categories`, {
-      method: 'GET'
+    method: "GET",
   })
-      .then(response => {
-          return response.json();
-      })
-      .catch(err => console.log(err));
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+export const listOrders = (userId, token) => {
+  return fetch(`${API}/order/list/${userId}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+export const getStatusValues = (userId, token) => {
+  return fetch(`${API}/order/status-values/${userId}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+export const updateOrderStatus = (userId, token, orderId, status) => {
+  return fetch(`${API}/order/${orderId}/status/${userId}`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ status, orderId }),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
 };
