956a33ef37343ec14e5d6b472648f348d6343d33
Khaled <khaled.ali.architect@gmail>
2023-07-06 19:47:59 +0300

    React - Admin Management of Orders and Products
    See: #48

--- client/src/Routing.js
+++ client/src/Routing.js
@@ -14,6 +14,9 @@ import Product from "./core/Product";
 import Cart from "./core/Cart";
 import Orders from "./admin/Orders";
 import Profile from "./user/Profile";
+import ManageProducts from "./admin/ManageProducts";
+import UpdateProduct from "./admin/UpdateProduct";
+import UpdateCategory from "./admin/updateCategory";
 
 const Routing = () => {
   return (
@@ -23,14 +26,25 @@ const Routing = () => {
         <Route path="/shop" exact component={Shop} />
         <Route path="/signin" exact component={Signin} />
         <Route path="/signup" exact component={Signup} />
+        <Route path="/product/:productId" exact component={Product} />
+        <Route path="/cart" exact component={Cart} />
         <PrivateRoute path="/user/dashboard" exact component={Dashboard} />
         <PrivateRoute path="/profile/:userId" exact component={Profile} />
+        <AdminRoute path="/admin/products" exact component={ManageProducts} />
         <AdminRoute path="/admin/dashboard" exact component={AdminDashboard} />
         <AdminRoute path="/create/category" exact component={AddCategory} />
         <AdminRoute path="/create/product" exact component={AddProduct} />
         <AdminRoute path="/admin/orders" exact component={Orders} />
-        <Route path="/product/:productId" exact component={Product} />
-        <Route path="/cart" exact component={Cart} />
+        <AdminRoute
+          path="/admin/product/update/:productId"
+          exact
+          component={UpdateProduct}
+        />
+        <AdminRoute
+          path="/admin/category/update/:categoryId"
+          exact
+          component={UpdateCategory}
+        />
       </Switch>
     </BrowserRouter>
   );
